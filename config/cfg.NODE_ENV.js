71e9bbb38b932f1a68c57b89eb4013f0272167d0
Baltazar Ponte <bponte@gmail.com>
2015-11-16 15:05:13 -0300

    add getTokenCloudCredentials, getRoleSize , getLocations

--- config/cfg.NODE_ENV.js
+++ config/cfg.NODE_ENV.js
@@ -7,4 +7,14 @@ config.storage.container = 'test';
 config.storage.account = 'azure-account-name-here';
 config.storage.accessKey = 'azure-storage-access-key-here';
 
+config.param={
+    tenant : '<TENANT>',
+    authorityHostUrl : 'https://login.windows.net',
+    clientId : '<CLIENT_ID>',
+    username : '<USERNAME>',
+    password : '<PASSWORD>',
+    cert: 'cert.pem',
+    key: 'key.pem'
+
+}
 module.exports = config;
